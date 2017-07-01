e67c6f7c7592537fa668597ef80164dbb581edea
Wojo <matt.wojtkun@gmail.com>
2017-07-01 13:15:03 -0400

    styled comment form and comment display

--- public/blog_script.js
+++ public/blog_script.js
@@ -1,3 +1,14 @@
 $(document).ready(function($) {
   console.log("jQuery ready");
+
+  // listen to add comment button
+  // add button onclick
+  //   disable add button
+  //   create div with id
+  // <form class="" action="/add_comment" method="post">
+  //   <input type="text" name="text" maxlength="150" placeholder="Write comment...">
+  //   <input type="hidden" name="post_id" id="current_post_id" value="<%= post.id %>">
+  //   <input type="submit" name="submit_comment" value="Submit">
+  // </form>
+
 });
