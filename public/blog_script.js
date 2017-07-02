363ad5fe35e0fa087ffe63bb4a712bf2904450d0
Wojo <matt.wojtkun@gmail.com>
2017-07-01 20:00:44 -0400

    added validation for sign-in and registration. closes #12

--- public/blog_script.js
+++ public/blog_script.js
@@ -1,14 +1,28 @@
 $(document).ready(function($) {
   console.log("jQuery ready");
 
-  // listen to add comment button
-  // add button onclick
-  //   disable add button
-  //   create div with id
-  // <form class="" action="/add_comment" method="post">
-  //   <input type="text" name="text" maxlength="150" placeholder="Write comment...">
-  //   <input type="hidden" name="post_id" id="current_post_id" value="<%= post.id %>">
-  //   <input type="submit" name="submit_comment" value="Submit">
-  // </form>
+  $('#signin-submit').on('click', function(){
+    if ($('.signin-input').val() == ""){
+      alert('Please enter a Username and password.');
+      return false
+    }
+  });
+
+  $('#registration-submit').on('click', function(){
+    if ($('.registration-input').val() == ""){
+      alert('You must fill out all fields.');
+      return false
+    } else {
+      var x = $('#registration-email').val();
+      var atpos = x.indexOf("@");
+      var dotpos = x.lastIndexOf(".");
+      if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Please enter a valid email address");
+        return false;
+      }
+    }
+  });
+
+
 
 });
